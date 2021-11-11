26ca058016a4b9323e5291d6878c7b3dda21e338
davidjosephind <davidjosephind@gmail.com>
2021-11-12 00:16:50 +0530

    Update book schema

--- models/Book.js
+++ models/Book.js
@@ -1,28 +1,16 @@
 const mongoose = require("mongoose");
 
-const BookSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Please add a name"],
-      unique: true,
-      trim: true,
-      maxlength: [50, "Name can not be more than 50 characters"],
-    },
-    description: {
-      type: String,
-      required: [true, "Please add a description"],
-      maxlength: [500, "Description can not be more than 500 characters"],
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
+const BookSchema = new mongoose.Schema({
+  name: {
+    type: String,
   },
-  {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-  }
-);
+  description: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model("Book", BookSchema);
